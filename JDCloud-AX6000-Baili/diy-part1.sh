9d38a1d57ff9e6f18d015efe9af7a63b72dbf265
kmdzjx <kf@kmdzjx.com>
2024-08-30 13:05:51 +0800

    Update diy-part1.sh

--- JDCloud-AX6000-Baili/diy-part1.sh
+++ JDCloud-AX6000-Baili/diy-part1.sh
@@ -11,15 +11,15 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # form helloworld repo
-# sed -i "/helloworld/d" "feeds.conf.default"
-# echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+sed -i "/helloworld/d" "feeds.conf.default"
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 
 git clone https://github.com/messense/aliyundrive-webdav package/messense
 git clone https://github.com/gdy666/luci-app-lucky package/lucky
